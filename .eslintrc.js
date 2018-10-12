4d96a95ddbb03949610e2b90708533b40f37effb
slavikzdebskyy <zdebskyy.slavik@gmail.com>
2018-10-13 02:12:43 +0300

    add functions: checkSimpleArr(arr), checkColumnsAndRows(arr), createBoard(arr), getBoard(), rotateRows(arr, firstIndex, secondIndex)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,52 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node": true
-  },
-  "extends": [
-    "eslint:recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+    "env": {
+      "browser": true,
+      "es6": true,
+      "node": true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "settings": {},
-  "plugins": [],
-  "rules": {
-    "eqeqeq": ["error", "always"],
-    "arrow-parens": ["error", "as-needed"],
-    "indent": [
-      "error",
-      2,
-      {
-        "SwitchCase": 1,
-        "ignoredNodes": [ "JSXAttribute", "JSXSpreadAttribute", ],
-      }
+    "extends": [
+      "eslint:recommended"
     ],
-    "max-len": [
-      "error",
-      {
-        "ignoreTrailingComments": true,
-        "ignoreComments": true,
-        "code": 100
-      }
-    ],
-    "linebreak-style": [
-      0,
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
-  }
-};
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "settings": {},
+    "plugins": [],
+    "rules": {
+        "eqeqeq": ["error", "always"],
+        "arrow-`parens`": ["error", "as-needed"],
+        "indent": [
+          "error",
+          2,
+          {
+            "SwitchCase": 1,
+            "ignoredNodes": [ "JSXAttribute", "JSXSpreadAttribute", ],
+          }
+        ],
+        "max-len": [
+          "error",
+          {
+            "ignoreTrailingComments": true,
+            "ignoreComments": true,
+            "code": 100
+          }
+        ],
+        "linebreak-style": [
+            0,
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+  };
+  
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-'use strict'
 
-const $field = document.getElementById('field');
-const $cells = $field.getElementsByClassName('cell');
 
-$field.addEventListener('keydown', ({target, keyCode}) => { // validation for Inputs: user can enter only numbers
-  console.log(keyCode);
+'use strict';
+
+const $field = document.getElementById('field');  //  div => game board
+
+const net = [
+  [1,2,3,4,5,6,7,8,9],
+  [4,5,6,7,8,9,1,2,3],
+  [7,8,9,1,2,3,4,5,6],
+  [2,3,4,5,6,7,8,9,1],
+  [5,6,7,8,9,1,2,3,4],
+  [8,9,1,2,3,4,5,6,7],
+  [3,4,5,6,7,8,9,1,2],
+  [6,7,8,9,1,2,3,4,5],
+  [9,1,2,3,4,5,6,7,8]  
+];
+
+
+//  input's validation (user can enter only numbers):
+$field.addEventListener('keydown', ({target, keyCode}) => { 
   target.onkeypress = () => {
     if(!(keyCode >= 49 && keyCode <= 57)){
       return false;
@@ -13,6 +27,109 @@ $field.addEventListener('keydown', ({target, keyCode}) => { // validation for In
       return false;
     }
     return true;    
-  }
-})
+  };
+});// END of input's validation
 
+//  function checkSimpleArr(arr) checks whether 
+//  all the elements are unique in a simple array:
+const checkSimpleArr = array => { 
+  const len = array.length;
+  for(let i = 0; i < len; i++){
+    for(let j = i + 1; j < len; j++){
+      if(array[i] === array[j]){
+        return false;
+      }
+    }
+  }
+  return true;
+};//  END function checkSimpleArr(arr)
+
+
+//  function checkColumnsAndRows(arr) checks whether 
+//  all the elements are unique in rows and columns of array:
+const checkColumnsAndRows = array => {
+  let j = 0;
+  let column;
+  const len = array.length;
+  do {
+    for(let i = 0; i < len; i++){
+      if(!checkSimpleArr(array[i])){
+        return false;
+      }
+      column = array.map(el=>{
+        return el[j];
+      });     
+    }
+    if(!checkSimpleArr(column)){      
+      return false;
+    }
+    j++;    
+  } while(j < len);  
+  return true;
+};//  END of function checkColumnsAndRows(arr)
+
+//  function createBoard(arr) creates HTML tags
+//  and rendered start board:
+const createBoard = array => {
+  array.forEach((el, index) => {
+    el.forEach(elChild => {
+      const input = document.createElement('input');
+      if(index === 2 || index === 5){
+        input.className = 'cell brdr-bttm';
+      } else {
+        input.className = 'cell';
+      }
+      if(elChild){
+        input.disabled = true;
+        input.value = elChild;
+      }
+      $field.appendChild(input);
+    });
+  });  
+};//  END of function createBoard(arr)
+
+//  function getBoard() returns array from game board
+//  that the user has filled in
+const getBoard = () => {
+  const $cells = $field.getElementsByClassName('cell');
+  const len = $cells.length;
+  const board = [];
+  let row =[];  
+  let koef = 1;
+  for(let i = 0; i < len; i++){
+    if((9 * koef) > i){
+      row.push($cells[i].value);      
+    } else {
+      board.push(row);
+      koef++;
+      row = [];
+      row.push($cells[i].value);
+    }        
+  }
+  board.push(row);
+  return board;
+};//  END function getBoard()
+
+//  function rotateRows(arr, firstIndex, secondIndex) rotates rows
+const rotateRows = (array, firstIndex, secondIndex) => {   
+  if((firstIndex >= 0 && secondIndex <=2) ||
+    (firstIndex >= 3 && secondIndex <=5) ||
+    (firstIndex >= 6 && secondIndex <=8)){
+    const el = array[firstIndex]; 
+    array.splice(firstIndex, 1, array[secondIndex]);
+    array.splice(secondIndex, 1, el);
+  }
+  else {
+    return array;
+  }
+};//  END of function rotateRows(arr, firstIndex, secondIndex)
+
+
+
+
+
+
+
+// rotateRows(net,6,7);
+
+createBoard(net);
